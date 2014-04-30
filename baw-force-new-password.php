5b38c0e353fadad6b9aed51cb0e84d669898fc7d
juliobox <juliobox@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-30 13:51:12 +0000

    v 1.0
    git-svn-id: https://plugins.svn.wordpress.org/baw-force-new-password/trunk@905610 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ baw-force-new-password.php
@@ -0,0 +1,45 @@
+<?php
+/*
+Plugin Name: Baw Force New Password
+Description: Force new users to change their password on their first login.
+Plugin URI: http://blog.secupress.fr/nouvel-utilisateur-nouveau-mot-de-passe-186.html
+Version: 1.0
+Author: Julio Potier
+Author URI: http://boiteaweb.fr
+License: GPL v2 or later
+*/
+
+add_action( 'init', 'bawfnp_redirect' );
+function bawfnp_redirect() {
+	global $current_user, $pagenow;
+	$current_user = wp_get_current_user();
+	$_pagenow = is_admin() && $pagenow ? $pagenow : null;
+	if ( 'profile.php' != $_pagenow && is_user_logged_in() && get_user_meta( $current_user->ID, 'force-new-password', true ) ) {
+		wp_redirect( admin_url( 'profile.php' ) );
+		die();
+	}
+}
+
+add_action( 'user_register', 'bawfnp_user_register' );
+function bawfnp_user_register( $user_id ) {
+	add_user_meta( $user_id, 'force-new-password', true );
+}
+
+add_action( 'personal_options_update', 'bawfnp_personal_options_update' );
+add_action( 'edit_user_profile_update', 'bawfnp_personal_options_update' );
+function bawfnp_personal_options_update( $user_id ) {
+	if ( isset( $_POST['pass1'], $_POST['pass2'] ) && $_POST['pass1'] == $_POST['pass2'] ) {
+		delete_user_meta( $user_id, 'force-new-password' );
+	}
+}
+
+add_action( 'admin_notices', 'bawfnp_notices' );
+function bawfnp_notices() {
+	if ( get_user_meta( $GLOBALS['current_user']->ID, 'force-new-password', true ) ) {
+		printf(
+			'<div class="error"><p>%s %s</p></div>',
+			__( 'You&rsquo;re using the auto-generated password for your account. Would you like to change it to something easier to remember?' ),
+			__( '(required)' )
+			);
+	}
+}
---
+++ readme.txt
@@ -0,0 +1,29 @@
+=== Force New Password ===
+Contributors: juliobox, secupress
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=KJGT942XKWJ6W
+Tags: user, member, password, security
+Requires at least: 3.0
+Tested up to: 3.9
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Force your new users to change their first password when they log in.
+
+== Description ==
+
+When a user is created, he can receive his password by email. If he keep this one, his password is stored in plain text in a mail, this is not secure.
+
+Force your new users to change their first password when they log in!
+
+== Installation ==
+
+1. Upload the plugin content to the *"/wp-content/plugins/"* directory
+1. Activate the plugin through the *"Plugins"* menu in WordPress
+1. No settings page
+
+== Changelog ==
+
+= 1.0 =
+* 30 apr 2014
+* First release
